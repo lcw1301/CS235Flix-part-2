79e4dbd7e7a14f4309665caaccb2904a096a14fc
lcw1301 <chenweilam@gmail.com>
2020-11-01 18:26:46 +1300

    revert changes

--- movie_app/__init__.py
+++ movie_app/__init__.py
@@ -2,10 +2,6 @@ import os
 
 from flask import Flask
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, clear_mappers
-from sqlalchemy.pool import NullPool
-
 import movie_app.adapters.repository as repo
 from movie_app.adapters import memory_repository
 from movie_app.adapters.memory_repository import MemoryRepository, populate
@@ -24,15 +20,6 @@ def create_app(test_config=None):
     if app.config['REPOSITORY'] == 'memory':
         repo.repo_instance = memory_repository.MemoryRepository()
         memory_repository.populate(data_path, repo.repo_instance)
-    elif app.config['REPOSITORY'] == 'database':
-        database_uri = app.config['SQLALCHEMY_DATABASE_URI']
-        database_echo = app.config['SQLALCHEMY_ECHO']
-        database_engine = create_engine(database_uri, connect_args={"check_same_thread": False}, poolclass=NullPool,
-                                        echo=database_echo)
-
-        if app.config['TESTING'] == 'True' or len(database_engine.table_names()) == 0:
-            print("REPOPULATING DATABASE")
-            clear_mappers()
 
     repo.repo_instance = MemoryRepository()
     populate(data_path, repo.repo_instance)
